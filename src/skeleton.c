c2b87f584b526bec60485ad95ce48904598058ef
dmolanor <dmolanor@gmail.com>
2021-02-20 12:45:33 +0100

    Init (2)

---
+++ src/skeleton.c
@@ -0,0 +1,55 @@
+/* 
+ * This file is part of the Sipeed Longan Nano 2663 Skeleton.
+ * Copyright (c) 2021 Dionisio Molano Robledo.
+ * 
+ * This program is free software: you can redistribute it and/or modify  
+ * it under the terms of the GNU General Public License as published by  
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License 
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "skeleton.h"
+
+// ---------------------------------------------------------------------
+// Private Constants
+// ---------------------------------------------------------------------
+/*!
+    \brief      function
+*/
+
+// ---------------------------------------------------------------------
+// Private Prototypes
+// ---------------------------------------------------------------------
+/*!
+    \brief      function
+    \param[in]  none
+    \param[out] none
+    \retval     system error
+*/
+
+// ---------------------------------------------------------------------
+// Public Bodies
+// ---------------------------------------------------------------------
+/*!
+    \brief      main function
+    \param[in]  none
+    \param[out] none
+    \retval     none
+*/
+
+// ---------------------------------------------------------------------
+// Private Bodies
+// ---------------------------------------------------------------------
+/*!
+    \brief      main function
+    \param[in]  none
+    \param[out] none
+    \retval     none
+*/
